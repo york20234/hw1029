977991ed9232f57f0c2f5cc015cd6ed146a4853e
york20234 <107021246@gm.asia.edu.tw>
2018-10-29 20:07:26 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //	Scanner scn = new Scanner(System.in);
+//	int m = scn.nextInt();
+//	switch (m){
+//        case 1:
+//            System.out.println("July");
+//            break;
+//        case 2:
+//            System.out.println("February");
+//            break;
+//        case 3:
+//            System.out.println("March ");
+//            break;
+//        case 4:
+//            System.out.println("Apri");
+//            break;
+//        case 5:
+//            System.out.println("May ");
+//            break;
+//        case 6:
+//            System.out.println("June");
+//            break;
+//        case 7:
+//            System.out.println("July");
+//            break;
+//        case 8:
+//            System.out.println(" August");
+//            break;
+//        case 9:
+//            System.out.println("September");
+//            break;
+//        case 10:
+//            System.out.println("October");
+//            break;
+//        case 11:
+//            System.out.println("November");
+//            break;
+//        case 12:
+//            System.out.println("December");
+//            break; }
+
+//        Scanner scn = new Scanner(System.in);
+//        String str = scn.next();
+//        switch (str) {
+//            case "Spring":
+//                System.out.println("2~4月");
+//                break;
+//            case "Summer":
+//                System.out.println("5~7月");
+//                break;
+//            case "Autumn":
+//                System.out.println("8~10月");
+//                break;
+//            case "Winter":
+//                System.out.println("11~12月&1月");
+//                break;
+//        }
+
+//        Scanner scn = new Scanner(System.in);
+//        int a = scn.nextInt();
+//        int b = scn.nextInt();
+//        int c = a%2;
+//        int d = b%2;
+//        int result=0;
+//        if (c==1&&d==1){
+//            for (int i =a;i<=b;i=i+2){
+//                result=result+i;}
+//                System.out.println(result); }
+//            else if (c==0&&d==1){
+//            for (int i =a+1;i<=b;i=i+2){
+//                result=result+i;}
+//                System.out.println(result); }
+//        else if (c==1&&d==0){
+//            for (int i =a;i<=b-1;i=i+2){
+//                result=result+i;}
+//                System.out.println(result);}
+//        else if (c==0&&d==0){
+//            for (int i =a+1;i<=b-1;i=i+2){
+//                result=result+i;}
+//                System.out.println(result); }
+
+//        Scanner scn = new Scanner(System.in);
+//        int a = scn.nextInt();
+//        int b = scn.nextInt();
+//        int c = a%2;
+//        int d = b%2;
+//        int result=0;
+//        if (c==0&&d==0){
+//            for (int i =a;i<=b;i=i+2){
+//                result=result+i;}
+//                System.out.println(result); }
+//            else if (c==0&&d==1){
+//            for (int i =a;i<=b-1;i=i+2){
+//               result=result+i;}
+//                System.out.println(result); }
+//        else if (c==1&&d==0){
+//            for (int i =a+1;i<=b;i=i+2){
+//                result=result+i;}
+//                System.out.println(result);}
+//        else if (c==1&&d==1){
+//            for (int i =a+1;i<=b-1;i=i+2){
+//                result=result+i;}
+//                System.out.println(result); }
+//.
+
+    }
+}
+    }
+}
